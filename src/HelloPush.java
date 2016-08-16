5ab012fa47e0edc108be0753204619d22fe08b09
sam <shf1601@iolani.org>
2016-08-15 20:03:12 -1000

    Updated HelloPush class

--- src/HelloPush.java
+++ src/HelloPush.java
@@ -1,6 +1,11 @@
+import math.Calculator;
 
 public class HelloPush {
 	public static void main(String[] args){
 		System.out.println("What's up");
+		Calculator calc = new Calculator();
+		int sum = calc.add(5, 5);
+		System.out.println("" + sum);
+		System.out.println(""+calc.fibonnaci(1, 1, 100));
 	}
 }
