aef1e52ac798c292dbb8fa6cf58364234ceed13b
sam <shf1601@iolani.org>
2016-08-15 20:22:56 -1000

    Added factorial line

--- src/HelloPush.java
+++ src/HelloPush.java
@@ -7,6 +7,7 @@ public class HelloPush {
 		int sum = calc.add(5, 5);
 		System.out.println("" + sum);
 		System.out.println(""+calc.fibonnaci(1, 1, 100));
-		System.out.println("what");
+		int fact = calc.factorial(9);
+		System.out.println(""+fact);
 	}
 }
