db88da59ad9c2247c48fd14c5f614f30e2e3ed97
sam <shf1601@iolani.org>
2016-08-15 20:03:22 -1000

    Created Calculator class

---
+++ src/math/Calculator.java
@@ -0,0 +1,26 @@
+package math;
+
+public class Calculator {
+	public Calculator(){
+		
+	}
+	
+	public int add(int n, int m){
+		return n + m;
+	}
+	
+	public int factorial(int n){
+		if(n == 1)
+			return 1;
+		else 
+			return n*factorial(n-1);
+	}
+	
+	public int fibonnaci(int n, int m, int limit){
+		int sum = n+m;
+		if(sum > limit)
+			return sum;
+		else
+			return fibonnaci(m, sum, limit);
+	}
+}
